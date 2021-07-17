bdf5e9ed71b47dbc416e76a30ad24efb6dc7267a
carloschicas <carlos.chicas@503consulting.com>
2021-07-17 14:08:57 -0600

    Add categories edit UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,21 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      flash[:notice] = 'Category was updated successfully.'
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
+
   def new
     @category = Category.new
   end
@@ -21,6 +37,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   before_action :require_user, only: [:edit, :update]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   
-  def show 
+  def show
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
